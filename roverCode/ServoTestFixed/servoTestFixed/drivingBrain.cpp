a981a9214a14e3825b5e021482496217552adab3
craigaloewen <craigaloewen@gmail.com>
2017-03-12 20:20:48 -0400

    Added driving functionability

--- roverCode/ServoTestFixed/servoTestFixed/drivingBrain.cpp
+++ roverCode/ServoTestFixed/servoTestFixed/drivingBrain.cpp
@@ -6,6 +6,10 @@ drivingBrain::drivingBrain(Servo* inLeftServo, Servo* inRightServo)
 {
   leftServo = inLeftServo;
   rightServo = inRightServo;
+
+  trimValue = INITIAL_TRIM_VALUE;
+
+  driveStop();
   
 }
 
@@ -17,8 +21,48 @@ drivingBrain::~drivingBrain()
 
 void drivingBrain::driveForwards(int power) {
 
+  
 
-  leftServo->writeMicroseconds(1000);
-  rightServo->writeMicroseconds(2000);
+  int leftServoMicroseconds = LEFT_STOP_SPEED + (LEFT_FULL_FORWARDS_SPEED - LEFT_STOP_SPEED) * (power / 100.0) * (1 - trimValue);
+  int rightServoMicroseconds = RIGHT_STOP_SPEED + (RIGHT_FULL_FORWARDS_SPEED - RIGHT_STOP_SPEED) * (power / 100.0) * (1 + trimValue);
+
+  
+  leftServo->writeMicroseconds(leftServoMicroseconds);
+  rightServo->writeMicroseconds(rightServoMicroseconds);
 }
+
+void drivingBrain::driveStop() {
+  leftServo->writeMicroseconds(LEFT_STOP_SPEED);
+  rightServo->writeMicroseconds(RIGHT_STOP_SPEED);
+}
+
+void drivingBrain::rotateAtSpeed(int power) {
+
+  int leftServoMicroseconds = LEFT_STOP_SPEED - (LEFT_FULL_FORWARDS_SPEED - LEFT_STOP_SPEED) * (power / 100.0) * (1 - trimValue);
+  int rightServoMicroseconds = RIGHT_STOP_SPEED + (RIGHT_FULL_FORWARDS_SPEED - RIGHT_STOP_SPEED) * (power / 100.0) * (1 + trimValue);
+
+  
+  leftServo->writeMicroseconds(leftServoMicroseconds);
+  rightServo->writeMicroseconds(rightServoMicroseconds);
+
+}
+
+void drivingBrain::rotateDegrees(int inDegrees) {
+
+  if (inDegrees > 0) {
+    rotateAtSpeed(90);
+  } else {
+    rotateAtSpeed(-90);
+    inDegrees = -inDegrees;
+  }
+
+  int delayAmount = (inDegrees / 90.0) * DEG_90_TURN_DELAY;
+  
+  delay(delayAmount);
+
+  driveStop();
+  
+}
+
+
 
--- roverCode/ServoTestFixed/servoTestFixed/servoTestFixed.ino
+++ roverCode/ServoTestFixed/servoTestFixed/servoTestFixed.ino
@@ -5,46 +5,27 @@
 #include "drivingBrain.h"
 
 
-#define LEFT_MOTOR_PIN 10
-#define RIGHT_MOTOR_PIN 11
+#define LEFT_MOTOR_PIN 11
+#define RIGHT_MOTOR_PIN 10
 #define BLIPPER_SERVO_PIN 9
 
-Servo servoL;
-Servo servoR;
-Servo servoBlipper; 
+
 blipperBrain* blipper;
 drivingBrain* wheelDriver;
 
 
 
-
-void blinkWait(unsigned int waitTime, unsigned int blinkTime) {
-
-  int totalTime = 0;
-
-  while (totalTime < waitTime) {
-    digitalWrite(LED_BUILTIN,HIGH);
-    delay(blinkTime/2);
-    digitalWrite(LED_BUILTIN,LOW);
-    delay(blinkTime/2);
-    totalTime += blinkTime;
-  }
-
-  
-}
-
-
 void setup() {
-
-  pinMode(LED_BUILTIN, OUTPUT);
+  
   Serial.begin(9600);
-  servoL.attach(LEFT_MOTOR_PIN);
-  servoR.attach(RIGHT_MOTOR_PIN);
-  servoBlipper.attach(BLIPPER_SERVO_PIN); 
 
-  Servo* servoSensor_p = (&servoBlipper);
-  Servo* servoL_p = (&servoL);
-  Servo* servoR_p = (&servoR);
+  Servo* servoL_p = new Servo();
+  Servo* servoR_p = new Servo();
+  Servo* servoSensor_p = new Servo();
+  
+  servoL_p->attach(LEFT_MOTOR_PIN);
+  servoR_p->attach(RIGHT_MOTOR_PIN);
+  servoSensor_p->attach(BLIPPER_SERVO_PIN); 
   
   blipper = new blipperBrain(servoSensor_p);
   wheelDriver = new drivingBrain(servoL_p, servoR_p);
@@ -53,31 +34,10 @@ void setup() {
 }
 
 
-void getSerialInput() {
-
-int incomingValue;
-
-if (Serial.available() > 0) {
-    incomingValue = Serial.parseInt();
-    Serial.println(incomingValue);
-    servoR.writeMicroseconds(incomingValue);
-    //servoR.writeMicroseconds(incomingValue);
-  }
-
-  //blinkWait(200,100);
-  
-}
-
 void loop() {
 
-  //testingAlgorithms mainTest;
-  //mainTest.testFilterArray(blipper);
 
-  //Serial.println(blipper->getUltrasonicRead());
-
-  getSerialInput();
-
-  
-
+  testingAlgorithms* mainTest = new testingAlgorithms();
+  mainTest->rightServoTestMicroseconds(wheelDriver);
 
 }
