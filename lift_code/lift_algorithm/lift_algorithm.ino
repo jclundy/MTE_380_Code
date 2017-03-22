3631e4ac039bf7bf27a5b833a122c3b58846fb34
unknown <elizabethannmorrow@gmail.com>
2017-03-21 23:22:53 -0400

    removing params from algorithm file

--- lift_code/lift_algorithm/lift_algorithm.ino
+++ lift_code/lift_algorithm/lift_algorithm.ino
@@ -6,48 +6,6 @@ Servo servoRight;
 Servo servoLatch;
 Servo servoBridge;
 
-/*
-// Positional servo values
-const int SERVO_LATCH_LATCHED = 600;
-const int SERVO_LATCH_UNLATCHED = 2400;
-const int SERVO_BRIDGE_DOWN = 600;
-const int SERVO_BRIDGE_UP = 2400;
-
-// Continuous servo stop values
-const int SERVO_LEFT_STOP=92;
-const int SERVO_RIGHT_STOP=92;
-const int SERVO_LEFT_STOP_ms=1500;
-const int SERVO_RIGHT_STOP_ms=1500;
-
-// Servo.write values
-/*
-const int SERVO_LEFT_FORWARD = 102;
-const int SERVO_RIGHT_FORWARD = 82;
-const int SERVO_LEFT_REVERSE = 82;
-const int SERVO_RIGHT_REVERSE = 102;
-
-
-const int PUSH_BUTTON_PIN = 13;
-
-const int SWITCH_LEFT_PIN = 2;
-const int SWITCH_RIGHT_PIN = 4;
-const int SWITCH_HEIGHT_PIN = 12;
-
-const int COMMUNICATION_PIN = 3; 
-
-// Ultrasonic 
-const int US_VCC_PIN = 2; 
-const int US_TRIG_PIN = 7; 
-const int US_ECHO_PIN = 8; 
-const int US_GND_PIN = 5; 
-
-const int SERVO_RIGHT_PIN = 11;
-const int SERVO_LEFT_PIN  = 10;
-const int LATCH_PIN = 9;
-const int BRIDGE_PIN = 6;
-
-const int POT_PIN = 2; */
-
 int buttonVal =0;
 double potVal = 0;
 bool buttonPressed = false;
